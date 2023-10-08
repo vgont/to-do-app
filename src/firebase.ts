57f6d7a8e0aada461476dc6122a22fb234a3aacc
vgont <vgontijo34@gmail.com>
2023-10-08 19:48:58 -0300

    removing personal data

--- src/firebase.ts
+++ src/firebase.ts
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBlrKWd38A-lLc1hhmOtzUBVC-ivLN8Y7E",
-  authDomain: "todo-list-46ecc.firebaseapp.com",
-  projectId: "todo-list-46ecc",
-  storageBucket: "todo-list-46ecc.appspot.com",
-  messagingSenderId: "373379564867",
-  appId: "1:373379564867:web:fc850d5f481724b55e9bf6"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 // Initialize Firebase
