1477436d992754233ec4b75ac1989c7bda388f9d
vgont <vgontijo34@gmail.com>
2023-10-08 22:21:01 -0300

    import fixed

--- src/firebase.ts
+++ src/firebase.ts
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  apiKey: "AIzaSyBlrKWd38A-lLc1hhmOtzUBVC-ivLN8Y7E",
+  authDomain: "todo-list-46ecc.firebaseapp.com",
+  projectId: "todo-list-46ecc",
+  storageBucket: "todo-list-46ecc.appspot.com",
+  messagingSenderId: "373379564867",
+  appId: "1:373379564867:web:fc850d5f481724b55e9bf6"
 };
 
 // Initialize Firebase
@@ -15,3 +15,4 @@ const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
 export { db };
+
